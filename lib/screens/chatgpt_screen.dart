348bb4a0ed4af6157e993002551d0bf344fcaca3
Raonson <ehsonmahmadmurodov@gmail.com>
2025-12-07 18:12:28 +0500

    Create chatgpt_screen.dart

---
+++ lib/screens/chatgpt_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChatGPTScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("ChatGPT Screen", style: TextStyle(color: Colors.white, fontSize: 24)),
+      ),
+    );
+  }
+}
