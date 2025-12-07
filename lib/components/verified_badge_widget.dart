a3a73f6e84ae9869888f70a10ef2d767702b1be8
Raonson <ehsonmahmadmurodov@gmail.com>
2025-12-07 18:24:53 +0500

    Create verified_badge_widget.dart

---
+++ lib/components/verified_badge_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class VerifiedBadgeWidget extends StatelessWidget {
+  final bool isVerified;
+  VerifiedBadgeWidget({required this.isVerified});
+
+  @override
+  Widget build(BuildContext context) {
+    return isVerified
+        ? Icon(Icons.check_circle, color: Colors.green)
+        : SizedBox.shrink();
+  }
+}
